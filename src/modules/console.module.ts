6252bbefd45984cc36ec8da5d0d121f3400ce6d2
Brent <r0888481@student.thomasmore.be>
2025-10-04 11:35:19 +0200

    Setting up Nest console module

---
+++ src/modules/console.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ConsoleModule as NestConsoleModule } from 'nestjs-console';
+import { PokemonModule } from '../modules/pokemon.module';
+import { SeedCommand } from './seeders/seed.command';
+import { ImportCommand } from './import.command';
+
+@Module({
+  imports: [NestConsoleModule, PokemonModule],
+  providers: [SeedCommand, ImportCommand],
+})
+export class ConsoleModule {}
