f2b1a738e7ae19c9866820a062888e341192c675
Brent <r0888481@student.thomasmore.be>
2025-10-04 13:16:40 +0200

    fixing bugs

--- src/modules/console.module.ts
+++ src/modules/console.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConsoleModule as NestConsoleModule } from 'nestjs-console';
 import { PokemonModule } from '../modules/pokemon.module';
-import { SeedCommand } from '../seeders/seed.command';
-import { ImportCommand } from './import.command';
+import { SeedCommand } from '../commands/seed.command';
+import { ImportCommand } from '../commands/import.command';
 
 @Module({
   imports: [NestConsoleModule, PokemonModule],
